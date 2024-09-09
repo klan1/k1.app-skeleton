9f2eb5027b943b90cdee286cb974e1b00e21a96f
j0hnd03 <alejo@klan1.com>
2024-09-09 10:22:52 -0500

    heading to v2.0

--- settings/app-paths-auto-def.php
+++ settings/app-paths-auto-def.php
@@ -10,18 +10,19 @@ define('k1app\K1APP_ROOT', str_replace('\\', '/', dirname(dirname(__FILE__))));
 define('k1app\K1APP_DIR', basename(K1APP_ROOT) . '/');
 define('k1app\K1APP_DOMAIN', $_SERVER['HTTP_HOST']);
 
-define('k1app\K1APP_CONTROLLERS_PATH', K1APP_ROOT . '/src/controllers/');
-define('k1app\K1APP_CLASSES_PATH', K1APP_ROOT . '/src/classes/');
+define('k1app\K1APP_CONTROLLERS_PATH', K1APP_ROOT . '/src/controllers/'); // 2.0
+define('k1app\K1APP_CLASSES_PATH', K1APP_ROOT . '/src/classes/'); // 2.0
+define('k1app\K1APP_ASSETS_PATH', K1APP_ROOT . '/assets/'); // 2.0
+define('k1app\K1APP_ASSETS_IMAGES_PATH', K1APP_ASSETS_PATH . 'images/'); // 2.0
 
-define('k1app\K1APP_VIEWS_PATH', K1APP_ROOT . '/views/');
+// define('k1app\K1APP_VIEWS_PATH', K1APP_ROOT . '/views/');
 
-define('k1app\K1APP_VIEWS_CRUD_PATH', K1APP_VIEWS_PATH . '/k1lib.crud/');
-define('k1app\K1APP_RESOURCES_PATH', K1APP_ROOT . '/resources/');
+// define('k1app\K1APP_VIEWS_CRUD_PATH', K1APP_VIEWS_PATH . '/k1lib.crud/');
 define('k1app\K1APP_SETTINGS_PATH', K1APP_ROOT . '/settings/');
-define('k1app\K1APP_UPLOADS_PATH', K1APP_RESOURCES_PATH . 'uploads/');
-define('k1app\K1APP_SHELL_SCRIPTS_PATH', K1APP_RESOURCES_PATH . '/shell-scripts/');
-define('k1app\K1APP_TEMPLATES_PATH', K1APP_RESOURCES_PATH . '/templates/');
-define('k1app\K1APP_FONTS_PATH', K1APP_RESOURCES_PATH . 'fonts/');
+define('k1app\K1APP_UPLOADS_PATH', K1APP_ASSETS_PATH . 'uploads/');
+define('k1app\K1APP_SHELL_SCRIPTS_PATH', K1APP_ASSETS_PATH . '/shell-scripts/');
+// define('k1app\K1APP_TEMPLATES_PATH', K1APP_RESOURCES_PATH . '/templates/');
+define('k1app\K1APP_FONTS_PATH', K1APP_ASSETS_PATH . 'fonts/');
 
 /**
  * COMPOSER
@@ -33,7 +34,7 @@ define('k1app\COMPOSER_PACKAGES_PATH', K1APP_ROOT . 'vendor/');
 define('k1app\BOWER_PACKAGES_PATH', K1APP_ROOT . 'bower_components/');
 
 // INCLUDES PATH ADDITION
-set_include_path(K1APP_SETTINGS_PATH . PATH_SEPARATOR . K1APP_RESOURCES_PATH . '/includes' . PATH_SEPARATOR . get_include_path());
+set_include_path(K1APP_SETTINGS_PATH . PATH_SEPARATOR . K1APP_ASSETS_PATH . '/includes' . PATH_SEPARATOR . get_include_path());
 
 // AUTO CONFIGURATED URLS 
 if (K1APP_MODE != 'shell') {
@@ -52,13 +53,11 @@ if (K1APP_MODE != 'shell') {
     define('k1app\K1APP_DOMAIN_URL', '//' . K1APP_DOMAIN);
 
     define('k1app\K1APP_URL', K1APP_DOMAIN_URL . K1APP_BASE_URL);
-    define('k1app\K1APP_LOGIN_URL', K1APP_URL . 'K1APP/log/form/');
     define('k1app\K1APP_HOME_URL', K1APP_URL);
-    define('k1app\K1APP_CONTROLLERS_URL', K1APP_URL . 'controllers/');
-    define('k1app\K1APP_VIEWS_URL', K1APP_URL . 'views/');
-    define('k1app\K1APP_RESOURCES_URL', K1APP_URL . 'resources/');
-    define('k1app\K1APP_UPLOADS_URL', K1APP_RESOURCES_URL . 'uploads/');
-    define('k1app\K1APP_TEMPLATES_URL', K1APP_RESOURCES_URL . 'templates/');
+    define('k1app\K1APP_ASSETS_URL', K1APP_HOME_URL . 'assets/');
+    define('k1app\K1APP_IMAGES_URL', K1APP_ASSETS_URL . 'images/');
+    define('k1app\K1APP_UPLOADS_URL', K1APP_ASSETS_URL . 'uploads/');
+    define('k1app\K1APP_TEMPLATES_URL', K1APP_ASSETS_URL . 'templates/');
     //    define('k1app\K1APP_TEMPLATE_IMAGES_URL', K1APP_TEMPLATE_URL . 'img/');
 
     /**
--- src/classes/k1app/core/template/my_menu.php
+++ src/classes/k1app/core/template/my_menu.php
@@ -10,14 +10,14 @@ class my_menu extends menu
     {
         parent::__construct($menu_title);
 
-        $this->add_item('App home', '/', 'bi bi-house');
-        $item = $this->add_item('Layouts', '/', 'bi bi-grid-1x2-fill')->is_sub();
+        $this->add_item('App home', '/', 'bi bi-house', 'nav-index');
+        $item = $this->add_item('Layouts', '/', 'bi bi-grid-1x2-fill')->nav_is_sub();
         $sub_menu = new menu(null, true);
         $sub_menu->append_to($item);
 
-        $sub_menu->add_subitem('Standard', '/layout/standard/','nav-layout-standard')->is_active();
-        $sub_menu->add_subitem('1 Column', '#','nav-layout-1column');
-        $sub_menu->add_subitem('Vertical Navbar', '#','nav-layout-vertical-navbar');
-        $sub_menu->add_subitem('Horizontal Menu', '#','nav-layout-horizontal-menu');
+        $sub_menu->add_subitem('Standard', '/layout/standard/', 'nav-layout-standard');
+        $sub_menu->add_subitem('1 Column', '#', 'nav-layout-1column');
+        $sub_menu->add_subitem('Vertical Navbar', '#', 'nav-layout-vertical-navbar');
+        $sub_menu->add_subitem('Horizontal Menu', '#', 'nav-layout-horizontal-menu');
     }
 }
