80e2dcc6547837be602212c99551924496de45ae
j0hnd03 <alejo@klan1.com>
2024-09-09 16:55:51 -0500

    better constants

--- v1-old-code/controllers/index.php
+++ v1-old-code/controllers/index.php
@@ -4,4 +4,4 @@ namespace k1app;
 
 use k1lib\urlrewrite\url as url;
 
-\k1lib\html\html_header_go(url::do_url(APP_URL . 'app/'));
+\k1lib\html\html_header_go(url::do_url(\k1app\K1APP_BASE_URL . 'app/'));
