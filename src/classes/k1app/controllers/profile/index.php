7f81d869bc88d82ae0834eb2fa25e6e9124dbd7d
j0hnd03 <alejo@klan1.com>
2024-09-09 20:08:42 -0500

    heading to 2.0

---
+++ src/classes/k1app/controllers/profile/index.php
@@ -0,0 +1,46 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * MAIN INDEX CONTROLLER BOOTSTRAP
+ *
+ * PHP version 8.2
+ *
+ * @author          Alejandro Trujillo J. <alejo@klan1.com> <https://github.com/j0hnd03>
+ * @copyright       2013-2024 Alejandro Trujillo J.
+ * @license         Apache 2.0
+ * @version         2.0
+ * @since           File available since Release 0.1
+ */
+
+namespace k1app\controllers\profile;
+
+use k1app\core\template\my_menu;
+use k1app\core\template\my_sidebar_page;
+use k1app\template\mazer\examples\profile;
+use k1app\template\mazer\layouts\blank;
+use k1lib\app\controller;
+use const k1app\K1APP_IMAGES_URL;
+
+class index extends controller {
+
+    /**
+     * @var my_sidebar_page
+     */
+    static protected blank $tpl;
+
+    static function run() {
+
+        $tpl = new profile();
+
+        $my_menu = new my_menu('Sidebar Menu');
+        $tpl->set_menu($my_menu);
+
+        $tpl->sidebar_logo_img()->set_src(K1APP_IMAGES_URL . 'klan1.png')->set_style('height:3.2rem');
+
+        $tpl->menu()->q('#nav-profile-page')->nav_is_active();
+
+        echo $tpl->generate();
+    }
+}
--- src/classes/k1app/core/template/my_menu.php
+++ src/classes/k1app/core/template/my_menu.php
@@ -3,21 +3,25 @@
 namespace k1app\core\template;
 
 use k1app\template\mazer\components\app\sidebar\wrapper\sidebar_menu\menu;
+use const k1app\K1APP_BASE_URL;
 
-class my_menu extends menu
-{
-    function __construct($menu_title = NULL)
-    {
+class my_menu extends menu {
+
+    function __construct($menu_title = NULL) {
         parent::__construct($menu_title);
 
-        $this->add_item('App home', '/', 'bi bi-house', 'nav-index');
-        $item = $this->add_item('Layouts', '/', 'bi bi-grid-1x2-fill')->nav_is_sub();
+        $this->add_item('App home', K1APP_BASE_URL, 'bi bi-house', 'nav-index');
+        $this->add_item('Login', K1APP_BASE_URL . 'auth/login/', 'bi bi-person-badge-fill', 'nav-login');
+
+        $item = $this->add_item('Layouts', '#', 'bi bi-grid-1x2-fill')->nav_is_sub();
         $sub_menu = new menu(null, true);
         $sub_menu->append_to($item);
+        $sub_menu->add_subitem('Sidebar and page', K1APP_BASE_URL . 'layout/standard/', 'nav-sidebar-page');
+        $sub_menu->add_subitem('Blank page', K1APP_BASE_URL . 'layout/blank/', 'nav-blank-page');
 
-        $sub_menu->add_subitem('Standard', '/layout/standard/', 'nav-layout-standard');
-        $sub_menu->add_subitem('1 Column', '#', 'nav-layout-1column');
-        $sub_menu->add_subitem('Vertical Navbar', '#', 'nav-layout-vertical-navbar');
-        $sub_menu->add_subitem('Horizontal Menu', '#', 'nav-layout-horizontal-menu');
+        $item = $this->add_item('Examples', '#', 'bi bi-file-earmark-medical-fill')->nav_is_sub();
+        $sub_menu = new menu(null, true);
+        $sub_menu->append_to($item);
+        $sub_menu->add_subitem('Profile', K1APP_BASE_URL . 'profile/', 'nav-profile-page');
     }
 }
