bde448ff4bcdef593943f29c17d070fb8066b62a
j0hnd03 <alejo@klan1.com>
2025-06-26 16:35:44 -0500

    menu changes

--- src/classes/k1app/core/template/my_menu.php
+++ src/classes/k1app/core/template/my_menu.php
@@ -14,23 +14,30 @@ class my_menu
     {
         parent::__construct($menu_title);
 
+        /**
+         * MENU TITLE
+         */
         $this->add_menu_title('PHP MAZER EXAMPLES');
         $this->add_item('App home', K1APP_BASE_URL, 'bi bi-house', 'nav-index');
 
-        $item = $this->add_item('Layouts', '#', 'bi bi-grid-1x2-fill')->nav_is_sub();
-        $sub_menu = new menu(null, true);
-        $sub_menu->append_to($item);
-        $sub_menu->add_subitem('Blank page', K1APP_BASE_URL . 'layout/blank/', 'nav-blank-page');
-        $sub_menu->add_subitem('Single page', K1APP_BASE_URL . 'layout/single_page/', 'nav-sidebar-page');
-        $sub_menu->add_subitem('Sidebar and page', K1APP_BASE_URL . 'layout/sidebar_page/', 'nav-sidebar-page');
+        /**
+         * MENU SECTION - LAYOUTS
+         */
+        $submenu_layouts = $this->add_item('Layouts', '#', 'bi bi-grid-1x2-fill')->nav_is_sub();
+        $submenu_layouts->add_subitem('Blank page', K1APP_BASE_URL . 'layout/blank/', 'nav-blank-page');
+        $submenu_layouts->add_subitem('Single page', K1APP_BASE_URL . 'layout/single_page/', 'nav-sidebar-page');
+        $submenu_layouts->add_subitem('Sidebar and page', K1APP_BASE_URL . 'layout/sidebar_page/', 'nav-sidebar-page');
 
-        $item = $this->add_item('Examples', '#', 'bi bi-file-earmark-medical-fill')->nav_is_sub();
-        $sub_menu = new menu(null, true);
-        $sub_menu->append_to($item);
-        $sub_menu->add_subitem('Profile', K1APP_BASE_URL . 'profile/', 'nav-profile-page');
+        /**
+         * MENU SECTION - EXAMPLES
+         */
+        $submenu_exmaples = $this->add_item('Examples', '#', 'bi bi-file-earmark-medical-fill')->nav_is_sub();
+        $submenu_exmaples->add_subitem('Profile', K1APP_BASE_URL . 'profile/', 'nav-profile-page');
 
-        $this->add_menu_title('K1 APP DEMO');
-
+        /**
+         * MENU SECTION - LOGIN
+         * normal item no as sub item
+         */
         if (app_session::is_logged())
         {
             $this->add_item('Logout', K1APP_BASE_URL . 'auth/logout/', 'bi bi-door-open', 'nav-login');
@@ -38,28 +45,32 @@ class my_menu
         {
             $this->add_item('Login', K1APP_BASE_URL . 'auth/login/', 'bi bi-person-badge-fill', 'nav-login');
         }
-        $item = $this->add_item('CRUD', '#', 'bi bi bi-table')->nav_is_sub();
-        $sub_menu = new menu(null, true);
-        $sub_menu->append_to($item);
-        $sub_menu->add_subitem('Simple Table', K1APP_BASE_URL . 'crud/table_simple/', 'nav-simple-crud');
-        $sub_menu->add_subitem('Related Table', K1APP_BASE_URL . 'crud/table_related/', 'nav-related-crud');
-        $sub_menu->add_subitem('Uploads Table', K1APP_BASE_URL . 'crud/table_uploads/', 'nav-uploads-crud');
-        // $sub_menu->add_subitem('Blank page', K1APP_BASE_URL . 'layout/blank/', 'nav-blank-page');
 
+        /**
+         * MENU SECTION - CRUD
+         */
+        $submenu_crud = $this->add_item('CRUD', '#', 'bi bi bi-table')->nav_is_sub();
+        $submenu_crud->add_subitem('Simple Table', K1APP_BASE_URL . 'crud/table_simple/', 'nav-simple-crud');
+        $submenu_crud->add_subitem('Related Table', K1APP_BASE_URL . 'crud/table_related/', 'nav-related-crud');
+        $submenu_crud->add_subitem('Uploads Table', K1APP_BASE_URL . 'crud/table_uploads/', 'nav-uploads-crud');
+
+        /**
+         * MENU SECTION - CONTROL PANEL
+         */
         if (app_session::check_user_level(['god']))
         {
             $this->add_menu_title('CONTROL PANEL');
 
-            $item = $this->add_item('DB Configurator', '#', 'bi bi-database', 'nav-db-configurator')->nav_is_sub();
-            $sub_menu = new menu(null, true);
-            $sub_menu->append_to($item);
-            $sub_menu->add_subitem('Config a table', K1APP_BASE_URL . 'core/admin/select_table/', 'nav-config-table');
-            $sub_menu->add_subitem(
+            $submenu_config = $this->add_item('DB Configurator', '#', 'bi bi-database', 'nav-db-configurator')->nav_is_sub();
+            $submenu_config->add_subitem(
+                    'Config a table', K1APP_BASE_URL . 'core/admin/select_table/', 'nav-config-table'
+            );
+            $submenu_config->add_subitem(
                     'Export configuration', K1APP_BASE_URL . 'core/admin/export_db_configuration/',
                     'nav-export-configuration'
             );
-            $sub_menu->q('#a-nav-export-configuration')->set_attrib('target', '_export');
-            $sub_menu->add_subitem(
+            $submenu_config->q('#a-nav-export-configuration')->set_attrib('target', '_export');
+            $submenu_config->add_subitem(
                     'Import configuration', K1APP_BASE_URL . 'core/admin/import_db_configuration/',
                     'nav-export-configuration'
             );
